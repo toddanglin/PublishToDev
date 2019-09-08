2d0a9d60376cde612bc3a86f9ce03a67395e2713
Burke Holland <burkeholland@gmail.com>
2019-09-08 10:35:57 -0500

    Standardize on single quotes in JavaScript

--- publishtodev-extension/src/popup.ts
+++ publishtodev-extension/src/popup.ts
@@ -1,12 +1,12 @@
 // import * as moment from 'moment';
 import * as moment from 'moment-timezone';
-import flatpickr from "flatpickr";
-require("flatpickr/dist/flatpickr.min.css")
+import flatpickr from 'flatpickr';
+require('flatpickr/dist/flatpickr.min.css')
 declare var window;
 
-let articleId = "";
-let title = "";
-let userapikey = "";
+let articleId = '';
+let title = '';
+let userapikey = '';
 let now = moment(new Date());
 let minDate = now.toDate();
 let maxDate = now.add(30, 'days').toDate(); //Limit to 30 days max (for now)
@@ -26,9 +26,9 @@ const eleConfigureBtn = document.querySelector('#btnConfigure');
   };
 
   chrome.tabs.executeScript({
-    code: 'document.querySelector(".body").dataset["articleId"]'
+    code: `document.querySelector('.body').dataset['articleId']`
   }, (result) => {
-    articleId = <any>result || "";
+    articleId = <any>result || '';
     init();
   })
 
@@ -44,23 +44,23 @@ const eleConfigureBtn = document.querySelector('#btnConfigure');
 
       console.log(articleId);
 
-      if (articleId == undefined || articleId == "") {
-        showError("Oops. A DevTo article ID was not found on this page. Please navigate to an unpublished post and try again.");
+      if (articleId == undefined || articleId == '') {
+        showError('Oops. A DevTo article ID was not found on this page. Please navigate to an unpublished post and try again.');
         return;
       }
 
       document.querySelector('#id').textContent = articleId;
-      title = tabs[0].title.substr(0, tabs[0].title.lastIndexOf("-") - 1);
-      // Remove the DevTo "- Dev Community" added to title attribute
+      title = tabs[0].title.substr(0, tabs[0].title.lastIndexOf('-') - 1);
+      // Remove the DevTo '- Dev Community' added to title attribute
       document.querySelector('#title').textContent = title;
     });
 
     // Validate apikey setting is set
     chrome.storage.sync.get({
-      apikey: ""
+      apikey: ''
     }, (items: { apikey }) => {
-      if (items.apikey == "") {
-        showError("Please configure your dev.to user API key in the extension settings.", false, true);
+      if (items.apikey == '') {
+        showError('Please configure your dev.to user API key in the extension settings.', false, true);
         return;
       } else {
         userapikey = items.apikey;
@@ -70,8 +70,8 @@ const eleConfigureBtn = document.querySelector('#btnConfigure');
 
   async function schedulePost() {
     // Validate that the selected date is in the future
-    if (elePubTime.value == "") {
-      alert("Please choose a date and time before continuing.");
+    if (elePubTime.value == '') {
+      alert('Please choose a date and time before continuing.');
       return;
     }
     let pubtime = new Date(elePubTime.value);
@@ -79,7 +79,7 @@ const eleConfigureBtn = document.querySelector('#btnConfigure');
     // Show progress message/spinner and make REST call
     eleBody.classList.add('msg');
     eleBody.classList.add('progress');
-    eleMsg.textContent = "Scheduling post...";
+    eleMsg.textContent = 'Scheduling post...';
     const postdata = {
       id: articleId.toString(),
       apikey: userapikey,
@@ -93,7 +93,7 @@ const eleConfigureBtn = document.querySelector('#btnConfigure');
       },
       body: JSON.stringify(postdata)
     }
-    const url = "https://publishto-dev.azurewebsites.net/api/schedulePost";
+    const url = 'https://publishto-dev.azurewebsites.net/api/schedulePost';
     try {
       let resp = await fetch(url, request_options);
       if (resp.status !== 200) {
@@ -102,17 +102,17 @@ const eleConfigureBtn = document.querySelector('#btnConfigure');
 
       eleBody.classList.remove('progress');
       eleBody.classList.add('success');
-      eleMsg.textContent = `All set! Your post is scheduled to publish on ${moment.tz(pubtime, zone).format("Do MMMM YYYY, h:mm A z")}`;
+      eleMsg.textContent = `All set! Your post is scheduled to publish on ${moment.tz(pubtime, zone).format('Do MMMM YYYY, h:mm A z')}`;
     } catch (err) {
       showError(err, true);
     }
   }
 
   function showError(msg, showCloseButton?, showConfigureButton?) {
-    eleBody.className = "";
+    eleBody.className = '';
     eleBody.classList.add('msg');
     eleBody.classList.add('error');
-    eleMsg.textContent = msg || "Something is not right";
+    eleMsg.textContent = msg || 'Something is not right';
 
     if (showCloseButton) {
       eleCloseBtn.setAttribute('style', 'display:block;');
@@ -124,16 +124,16 @@ const eleConfigureBtn = document.querySelector('#btnConfigure');
   }
 
   function closeMsg() {
-    eleBody.className = "";
+    eleBody.className = '';
   }
 
   function openOptions() {
     chrome.tabs.create({'url': `chrome://extensions/?options=${chrome.runtime.id}`});
   }
 
-  eleCloseBtn.addEventListener("click", closeMsg);
-  eleConfigureBtn.addEventListener("click", openOptions);
-  document.querySelector('#schedule').addEventListener("click", schedulePost);
+  eleCloseBtn.addEventListener('click', closeMsg);
+  eleConfigureBtn.addEventListener('click', openOptions);
+  document.querySelector('#schedule').addEventListener('click', schedulePost);
   document.addEventListener('DOMContentLoaded', () => {
     // HACK CITY -- Couldn't for the life of me get the stupid date picker it init
     // I assume it has something to do with the way Chrome extension scripts/html are loaded
@@ -151,6 +151,6 @@ const eleConfigureBtn = document.querySelector('#btnConfigure');
     }, 100);
 
     let tz = document.querySelector('#timezone');
-    tz.textContent = `(${zone} ${moment.tz(new Date(), zone).format("Z z")})`;
+    tz.textContent = `(${zone} ${moment.tz(new Date(), zone).format('Z z')})`;
   });
 })();
